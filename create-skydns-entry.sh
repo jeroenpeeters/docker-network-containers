4fa01d7275cc7f0633de6e89529b8852b671044e
Jeroen Peeters <jeroen@peetersweb.nl>
2014-11-19 13:47:36 +0100

    Update create-skydns-entry.sh

--- create-skydns-entry.sh
+++ create-skydns-entry.sh
@@ -1,6 +1,5 @@
-
 cname=$1
 etcd_url=$2
 
 myip=$(docker logs publicnetwork-$cname | grep "bound to" | awk '{print $3}');
-curl -XPUT $etcd_url -d value='{"host":"$myip"}'
+curl -XPUT $etcd_url -d value={\"host\":\"$myip\"}
