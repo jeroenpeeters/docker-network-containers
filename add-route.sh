cac53ab9ebfb84f8ba94c009535d494a20b87921
Jeroen Peeters <jeroen.peeters@ictu.nl>
2015-02-25 16:27:31 +0100

    add/delete network route on the local host for a specific network container

---
+++ add-route.sh
@@ -0,0 +1,21 @@
+# This script creates a local route
+
+me=`basename $0`
+function usage(){
+  echo "Usage: $me CONTAINERNAME"
+  echo "   CONTAINERNAME    The name of a running container for which to create a network route"
+  exit 1
+}
+if [ $# -lt 1 ]; then
+  usage
+fi
+
+cname=$1
+name="publicnetwork-$cname"
+pubip=$(docker logs $name | grep "bound to" | awk '{print $3}');
+if [[ $pubip ]]; then
+  route add -host $pubip docker0
+  echo "Route on docker0 for $pubip added"
+else
+  echo "No network container found for $cname or no IP bound to network container."
+fi
