29730f5506be985a6f9f1a2560cd78dd4ae6e251
jeroenpeeters <jeroen.peeters@ictu.nl>
2015-04-10 08:55:13 +0200

    get ip from ifconfig using docker exec

--- create-skydns-entry.sh
+++ create-skydns-entry.sh
@@ -1,5 +1,5 @@
 cname=$1
 etcd_url=$2
 
-myip=$(docker logs publicnetwork-$cname | grep "bound to" | awk '{print $3}');
+myip=$(docker exec publicnetwork-$cname ifconfig eth1 | grep "inet addr:" | awk '{print $2}' | awk -F: '{print $2}')
 curl -XPUT $etcd_url -d value={\"host\":\"$myip\"}
